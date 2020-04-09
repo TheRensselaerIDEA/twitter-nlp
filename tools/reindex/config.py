00a47ddf309b5206cf091fb9464afb05f8a2be5b
Avi Sanders <abraham.sanders@gmail.com>
2020-04-09 00:48:37 -0400

    All tools now have their own lightweight configs instead of relying on twitter_monitor's config

---
+++ tools/reindex/config.py
@@ -0,0 +1,29 @@
+"""
+Config class containing all the settings for running reindex tool
+"""
+
+import jsonpickle
+
+class Config(object):
+    """Container for reindex tool settings.
+
+    """
+    def __init__(self):
+        """Initializes the Config instance.
+        """
+        #Elasticsearch settings
+        self.elasticsearch_host = ""
+        self.elasticsearch_verify_certs = False
+        self.elasticsearch_index_name = ""
+
+    @staticmethod
+    def load(filepath):
+        """Loads the config from a JSON file.
+
+        Args:
+            filepath: path of the JSON file.
+        """
+        with open(filepath, "r") as file:
+            json = file.read()
+        config = jsonpickle.decode(json)
+        return config
