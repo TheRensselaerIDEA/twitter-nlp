ee3b9b49bb65edb372ff21abf29382613bd4e52a
Avi Sanders <abraham.sanders@gmail.com>
2020-08-24 03:34:04 -0400

    Added experimental summarization notebook, and updated Summarizer.R to support multiple models

--- analysis/Summarizer.R
+++ analysis/Summarizer.R
@@ -7,18 +7,22 @@ summarize <- function(text,
                       max_len=60, 
                       num_beams=4,
                       temperature=1.0,
+                      model=NULL,
                       summarizer_url="http://localhost:8080/batchsummarize") {
   
   if (is.character(text) && length(text) == 1) {
     text = list(text)
   }
   
-  res <- POST(url=summarizer_url,
-              encode="json",
-              body=list(max_len = max_len, 
-                        num_beams = num_beams,
-                        temperature = temperature,
-                        text = text))
+  body <- list(max_len = max_len, 
+            num_beams = num_beams,
+            temperature = temperature,
+            text = text)
+  if (!is.null(model)) {
+    body$model <- model
+  }
+  
+  res <- POST(url=summarizer_url, encode="json", body=body)
   
   res.text <- unlist(content(res))
   return(res.text)
